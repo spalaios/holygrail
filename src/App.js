c9d2ca3704f48655e75afefe8e37cfe80db4511c
spalaios <palaisuraj@gmail.com>
2018-11-23 00:52:37 +0530

    starter pack for eslint + prettier + airbnb style guide

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+
+const App = () => (
+  <Fragment>
+    <p className="test">First para with prettier linting</p>
+    <p>second para</p>
+  </Fragment>
+);
+
+export default App;
